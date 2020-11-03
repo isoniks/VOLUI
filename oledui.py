093d30c42c6d9c2698598526ea3e7ff10ef43b62
isoniks <73814514+isoniks@users.noreply.github.com>
2020-11-03 12:42:23 +1100

    Update oledui.py

--- oledui.py
+++ oledui.py
@@ -33,11 +33,7 @@ volumioIO = SocketIO(volumio_host, volumio_port)
 
 STATE_NONE = -1
 STATE_PLAYER = 0
-STATE_PLAYLIST_MENU = 1
-STATE_QUEUE_MENU = 2
 STATE_VOLUME = 3
-STATE_SHOW_INFO = 4
-STATE_LIBRARY_MENU = 5
 
 UPDATE_INTERVAL = 0.034
 PIXEL_SHIFT_TIME = 120    #time between picture position shifts in sec.
@@ -113,18 +109,6 @@ def SetState(status):
         oled.modal.SetPlayingIcon(oled.playState, 0)
     elif oled.state == STATE_VOLUME:
         oled.modal = VolumeScreen(oled.HEIGHT, oled.WIDTH, oled.volume, font, font2)
-    elif oled.state == STATE_PLAYLIST_MENU:
-        oled.modal = MenuScreen(oled.HEIGHT, oled.WIDTH, font2, oled.playlistoptions, rows=3, label='------ Select Playlist ------')
-    elif oled.state == STATE_QUEUE_MENU:
-        oled.modal = MenuScreen(oled.HEIGHT, oled.WIDTH, font2, oled.queue, rows=4, selected=oled.playPosition, showIndex=True)
-    elif oled.state == STATE_LIBRARY_MENU:
-        oled.modal = MenuScreen(oled.HEIGHT, oled.WIDTH, font2, oled.libraryNames, rows=3, label='------ Music Library ------')
-
-def LoadPlaylist(playlistname):
-    print ("loading playlist: ", playlistname.encode('ascii', 'ignore'))
-    oled.playPosition = 0
-    volumioIO.emit('playPlaylist', {'name':playlistname})
-    SetState(STATE_PLAYER)
 
 def onPushState(data):
     #print(data)
@@ -173,58 +157,6 @@ def onPushState(data):
                 iconTime = 80
             oled.modal.SetPlayingIcon(oled.playState, iconTime)
 
-def onPushQueue(data):
-    oled.queue = [track['name'] if 'name' in track else 'no track' for track in data]
-    print('Queue length is ' + str(len(oled.queue)))
-
-def onPushBrowseSources(data):
-#    print('Browse sources:')
-#    for item in data:
-#        print(item['uri']) 
-    pass
-
-def onLibraryBrowse(data):
-    oled.libraryFull = data
-    itemList = oled.libraryFull['navigation']['lists'][0]['items']
-    oled.libraryNames = [item['title'] if 'title' in item else 'empty' for item in itemList]
-    SetState(STATE_LIBRARY_MENU)
-
-def EnterLibraryItem(itemNo):
-    try:
-        selectedItem = oled.libraryFull['navigation']['lists'][0]['items'][itemNo]
-    except IndexError:
-        LibraryReturn()
-    else:
-        print("Entering library item: ", oled.libraryNames[itemNo].encode('ascii', 'ignore'))
-        if selectedItem['type'][-8:] == 'category' or selectedItem['type'] == 'folder':
-            volumioIO.emit('browseLibrary',{'uri':selectedItem['uri']})
-        else:
-            print("Sending new Queue")
-            volumioIO.emit('clearQueue')        #clear queue and add whole list of items
-            oled.queue = []
-            volumioIO.emit('addToQueue', oled.libraryFull['navigation']['lists'][0]['items'])
-            oled.stateTimeout = 5.0       #maximum time to load new queue
-            while len(oled.queue) == 0 and oled.stateTimeout > 0.1:
-                sleep(0.1) 
-            oled.stateTimeout = 0.2
-            print("Play position = " + str(itemNo))
-            volumioIO.emit('play', {'value':itemNo})
-
-def LibraryReturn():        #go to parent category
-    if not 'prev' in oled.libraryFull['navigation']:
-        SetState(STATE_PLAYER)
-    else:
-        parentCategory = oled.libraryFull['navigation']['prev']['uri']
-        print ("Navigating to parent category in library: ", parentCategory.encode('ascii', 'ignore'))
-        if parentCategory != '' and parentCategory != '/': 
-            volumioIO.emit('browseLibrary',{'uri':parentCategory})
-        else:
-            SetState(STATE_PLAYER)
-
-def onPushListPlaylist(data):
-    global oled
-    if len(data) > 0:
-        oled.playlistoptions = data
 
 class NowPlayingScreen():
     def __init__(self, height, width, row1, row2, font, fontaw):
@@ -264,98 +196,6 @@ class NowPlayingScreen():
         drawalfa.text((left, 4), self.playingIcon, font=self.fontaw, fill=(255, 255, 255, 96))
         self.iconcountdown = time
 
-class VolumeScreen():
-    def __init__(self, height, width, volume, font, font2):
-        self.height = height
-        self.width = width
-        self.font = font
-        self.font2 = font2
-        self.volumeLabel = None
-        self.labelPos = (10, 5)
-        self.volumeNumber = None
-        self.numberPos = (10, 25)
-        self.barHeight = 22
-        self.barWidth = 140
-        self.volumeBar = Bar(self.height, self.width, self.barHeight, self.barWidth)
-        self.barPos = (85, 27)
-        self.volume = 0
-        self.DisplayVolume(volume)
-
-    def DisplayVolume(self, volume):
-        self.volume = volume
-        self.volumeNumber = StaticText(self.height, self.width, str(volume) + '%', self.font)
-        self.volumeLabel = StaticText(self.height, self.width, 'Volume', self.font2)
-        self.volumeBar.SetFilledPercentage(volume)
-
-    def DrawOn(self, image):
-        self.volumeLabel.DrawOn(image, self.labelPos)
-        self.volumeNumber.DrawOn(image, self.numberPos)
-        self.volumeBar.DrawOn(image, self.barPos)
-
-class MenuScreen():
-    def __init__(self, height, width, font2, menuList, selected=0, rows=3, label='', showIndex=False):
-        self.height = height
-        self.width = width
-        self.font2 = font2
-        self.selectedOption = selected
-        self.menuLabel = StaticText(self.height, self.width, label, self.font2, center=True)
-        if label == '':
-            self.hasLabel = 0
-        else:
-            self.hasLabel = 1
-        self.labelPos = (1, 3)
-        self.menuYPos = 3 + 16 * self.hasLabel
-        self.menurows = rows
-        self.menuText = [None for i in range(self.menurows)]
-        self.menuList = menuList
-        self.totaloptions = len(menuList)
-        self.onscreenoptions = min(self.menurows, self.totaloptions)
-        self.firstrowindex = 0
-        self.showIndex = showIndex
-        self.MenuUpdate()
-
-    def MenuUpdate(self):
-        self.firstrowindex = min(self.firstrowindex, self.selectedOption)
-        self.firstrowindex = max(self.firstrowindex, self.selectedOption - (self.menurows-1))
-        for row in range(self.onscreenoptions):
-            if (self.firstrowindex + row) == self.selectedOption:
-                color = "black"
-                bgcolor = "white"
-            else:
-                color = "white"
-                bgcolor = "black"
-            optionText = self.menuList[row+self.firstrowindex]
-            if self.showIndex:
-                width = 1 + len(str(self.totaloptions))      # more digits needs more space
-                optionText = '{0:{width}d} {1}'.format(row + self.firstrowindex + 1, optionText, width=width)
-            self.menuText[row] = StaticText(self.height, self.width, optionText, self.font2, fill=color, bgcolor=bgcolor)
-        if self.totaloptions == 0:
-            self.menuText[0] = StaticText(self.height, self.width, 'no items..', self.font2, fill="white", bgcolor="black")
-            
-    def NextOption(self):
-        self.selectedOption = min(self.selectedOption + 1, self.totaloptions - 1)
-        self.MenuUpdate()
-
-    def PrevOption(self):
-        self.selectedOption = max(self.selectedOption - 1, 0)
-        self.MenuUpdate()
-
-    def SelectedOption(self):
-        return self.selectedOption
-        
-    def UpdatePlayingInfo(self, row1, row2):
-        pass
-    def SetPlayingIcon(self, state, time=0):
-        pass
-
-    def DrawOn(self, image):
-        if self.hasLabel:
-            self.menuLabel.DrawOn(image, self.labelPos)
-        for row in range(self.onscreenoptions):
-            self.menuText[row].DrawOn(image, (5, self.menuYPos + row*16))
-        if self.totaloptions == 0:
-            self.menuText[0].DrawOn(image, (15, self.menuYPos))
-
 show_logo("volumio_logo.ppm", oled)
 sleep(2)
 SetState(STATE_PLAYER)
@@ -372,10 +212,6 @@ receive_thread.daemon = True
 receive_thread.start()
 
 volumioIO.on('pushState', onPushState)
-volumioIO.on('pushListPlaylist', onPushListPlaylist)
-volumioIO.on('pushQueue', onPushQueue)
-volumioIO.on('pushBrowseSources', onPushBrowseSources)
-volumioIO.on('pushBrowseLibrary', onLibraryBrowse)
 
 # get list of Playlists and initial state
 volumioIO.emit('listPlaylist')
